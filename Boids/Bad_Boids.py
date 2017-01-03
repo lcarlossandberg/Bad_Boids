1330e97e41c54ea1deae52699d72fa9d1d5ad24a
lcarlossandberg <carlossandberg@googlemail.com>
2017-01-03 16:49:21 +0000

    Added bird Var V1.0.4

--- Boids/Bad_Boids.py
+++ Boids/Bad_Boids.py
@@ -2,38 +2,38 @@ from matplotlib import pyplot as plt
 from matplotlib import animation
 import random
 
-# Deliberately terrible code for teaching purposes
+Birds = 50 #number of birds in the simulation
 
-boids_x=[random.uniform(-450,50.0) for x in range(50)]
-boids_y=[random.uniform(300.0,600.0) for x in range(50)]
-boid_x_velocities=[random.uniform(0,10.0) for x in range(50)]
-boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(50)]
+boids_x=[random.uniform(-450,50.0) for x in range(Birds)]
+boids_y=[random.uniform(300.0,600.0) for x in range(Birds)]
+boid_x_velocities=[random.uniform(0,10.0) for x in range(Birds)]
+boid_y_velocities=[random.uniform(-20.0,20.0) for x in range(Birds)]
 boids=(boids_x,boids_y,boid_x_velocities,boid_y_velocities)
 
 
 def update_boids(boids):
     xs,ys,xvs,yvs=boids
     # Fly towards the middle
-    for i in range(50):
-        for j in range(50):
+    for i in range(Birds):
+        for j in range(Birds):
             xvs[i]=xvs[i]+(xs[j]-xs[i])*0.01/len(xs)
-    for i in range(50):
-        for j in range(50):
+    for i in range(Birds):
+        for j in range(Birds):
             yvs[i]=yvs[i]+(ys[j]-ys[i])*0.01/len(xs)
     # Fly away from nearby boids
-    for i in range(50):
-        for j in range(50):
+    for i in range(Birds):
+        for j in range(Birds):
             if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 100:
                 xvs[i]=xvs[i]+(xs[i]-xs[j])
                 yvs[i]=yvs[i]+(ys[i]-ys[j])
     # Try to match speed with nearby boids
-    for i in range(50):
-        for j in range(50):
+    for i in range(Birds):
+        for j in range(Birds):
             if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < 10000:
                 xvs[i]=xvs[i]+(xvs[j]-xvs[i])*0.125/len(xs)
                 yvs[i]=yvs[i]+(yvs[j]-yvs[i])*0.125/len(xs)
     # Move according to velocities
-    for i in range(50):
+    for i in range(Birds):
         xs[i]=xs[i]+xvs[i]
         ys[i]=ys[i]+yvs[i]
 

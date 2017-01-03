cdf272ddd7b78ccc37f5458b1b1b8d56f93b0e6e
lcarlossandberg <carlossandberg@googlemail.com>
2017-01-03 13:44:41 +0000

    Fix indentation  V1.0.1

--- Bad_Boids.py
+++ Bad_Boids.py
@@ -44,8 +44,10 @@ axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
 scatter=axes.scatter(boids[0],boids[1])
 
 def animate(frame):
-    update_boids(boids) scatter.set_offsets(zip(boids[0],boids[1]))
-    anim = animation.FuncAnimation(figure, animate, frames=50, interval=50)
+    update_boids(boids)
+    scatter.set_offsets(zip(boids[0],boids[1]))
+
+anim = animation.FuncAnimation(figure, animate, frames=50, interval=50)
 
 if __name__ == "__main__":
     plt.show()

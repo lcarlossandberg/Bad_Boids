4ab2c39bd3e862320442ff77c0c13e37f089ebf8
lcarlossandberg <carlossandberg@googlemail.com>
2017-01-04 17:48:28 +0000

    ad functions in UpdateBoids V1.3.0

--- Boids/Bad_Boids.py
+++ Boids/Bad_Boids.py
@@ -20,13 +20,14 @@ class UpdateBoids(object):
         self.positions = positions
         self.velocities = velocities
 
-    def update_boids(self):
+    def to_middle(self):
         # Fly towards the middle
         strength_of_atraction = 0.01
         middle_of_flock = np.mean(self.positions, 1)
         direction_to_middle = self.positions - middle_of_flock[:, np.newaxis]
         self.velocities -= direction_to_middle*strength_of_atraction
 
+    def away_from(self):
         # Fly away from nearby boids
         separations = self.positions[:,np.newaxis,:] - self.positions[:,:,np.newaxis]
         squared_displacements = separations * separations
@@ -38,16 +39,23 @@ class UpdateBoids(object):
         separations_if_close[1,:,:][far_away] = 0
         self.velocities += np.sum(separations_if_close,1)
 
+    def match_speed(self):
         # Try to match speed with nearby boids
         velocity_separation = self.velocities[:,np.newaxis,:] - self.velocities[:,:,np.newaxis]
         formation_flying_distance = 10000
         formation_flying_strength = 0.125
+
+        separations = self.positions[:,np.newaxis,:] - self.positions[:,:,np.newaxis]
+        squared_displacements = separations * separations
+        square_distances = np.sum(squared_displacements, 0)
+
         very_far=square_distances > formation_flying_distance
         velocity_separation_if_close = np.copy(velocity_separation)
         velocity_separation_if_close[0,:,:][very_far] =0
         velocity_separation_if_close[1,:,:][very_far] =0
         self.velocities -= np.mean(velocity_separation_if_close, 1) * formation_flying_strength
     
+    def move_velocities(self):
         # Move according to velocities
         self.positions += self.velocities
 
@@ -56,9 +64,16 @@ figure=plt.figure()
 axes=plt.axes(xlim=(-500,1500), ylim=(-500,1500))
 scatter=axes.scatter(positions[0],positions[1])
 
+def update_boids(positions, velocities):
+    up_date = UpdateBoids(positions, velocities)
+    up_date.to_middle()
+    up_date.away_from()
+    up_date.match_speed()
+    up_date.move_velocities()
 
 def animate(frame):
-    UpdateBoids(positions, velocities).update_boids()
+    #UpdateBoids(positions, velocities).update_boids()
+    update_boids(positions, velocities)
     scatter.set_offsets(zip(positions[0],positions[1]))
 
 anim = animation.FuncAnimation(figure, animate, frames=50, interval=50)

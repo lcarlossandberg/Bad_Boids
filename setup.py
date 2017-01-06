87fa3c98c8d6e8eb87484bba2216f0d2a1482cf5
lcarlossandberg <carlossandberg@googlemail.com>
2017-01-06 14:31:11 +0000

    fix broken upload 4 V6.5.11

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-      name = "boid",
+      name = "boids",
       version = "2.7.13",
       packages = find_packages(exclude=['*test']),
       scripts = ['scripts/boids'],

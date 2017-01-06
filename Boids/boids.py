0ca0c29521c5911292f4d3523846c1bb434b4e09
lcarlossandberg <carlossandberg@googlemail.com>
2017-01-06 00:01:51 +0000

    Added file parser V6.2.0

--- Boids/boids.py
+++ Boids/boids.py
@@ -1,4 +1,6 @@
 import argparse
+import yaml
+from StringIO import StringIO
 from newboids import NewBoids
 from animateboids import AnimateBoids
 
@@ -6,6 +8,7 @@ from animateboids import AnimateBoids
 
 def parser():
     parser = argparse.ArgumentParser(description = 'This is a simulation of boids')
+    parser.add_argument('--argfile', type=str, required=False, help='This is used to select a file to be used to input the arguments, this file should have a dictionary in it')
     parser.add_argument('--count', type=int, default='50', required=False, help='The number of boids in the simulation')
     parser.add_argument('--attraction', type=float, default='0.01', required=False, help='The strenght of the attraction between the boids')
     parser.add_argument('--alert', type=float, default='100', required=False, help='The distance at which the boids avoid each other')
@@ -17,13 +20,26 @@ def parser():
     args=parser.parse_args()
 
 
-    
-    
-    boids = NewBoids(args.count).return_boids()
-    positions = boids[0]
-    velocities = boids[1]
 
-    AnimateBoids(positions, velocities, args.attraction, args.alert, args.formation, args.strength, (args.save, args.name)).display()
+    if args.argfile:
+        file_args = {}
+        open_file = open(args.argfile, 'r')
+        file_args = yaml.load(open_file)
+        open_file.close()
+
+
+        boids = NewBoids(file_args['count']).return_boids()
+        positions = boids[0]
+        velocities = boids[1]
+
+        AnimateBoids(positions, velocities, file_args['attraction'], file_args['alert'], file_args['formation'], file_args['strength'], (file_args['save'], file_args['name'])).display()
+    else:
+        
+        boids = NewBoids(args.count).return_boids()
+        positions = boids[0]
+        velocities = boids[1]
+
+        AnimateBoids(positions, velocities, args.attraction, args.alert, args.formation, args.strength, (args.save, args.name)).display()
 
 
 if __name__ == "__main__":

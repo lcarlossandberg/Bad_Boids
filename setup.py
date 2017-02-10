26cc1b24ef2616b36d01be99e1f7955a2baf651c
lcarlossandberg <carlossandberg@googlemail.com>
2017-02-10 13:56:29 +0000

    Editing documentation V6.6.2

--- setup.py
+++ setup.py
@@ -2,7 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
       name = "Boids",
-      version = "2.7.13",
+      version = "6.6.2",
+      description = "Simulation of Boids",
+      author = "Leo Carlos-Sandberg"
+      url = "https://github.com/lcarlossandberg/Bad_Boids",
+      license = "MIT License"
       packages = find_packages(exclude=['*test']),
       scripts = ['scripts/Boids'],
       
